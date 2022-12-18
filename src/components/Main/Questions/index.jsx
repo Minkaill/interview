a9d6384a04cb9749788b40744146439af9d8ba6b
Wiynxr <mkx.dake@gmail.com>
2022-12-18 05:59:54 +0300

    Добавил отображение ответов(response)

--- src/components/Main/Questions/index.jsx
+++ src/components/Main/Questions/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Questions = ({
   id,
   title,
@@ -6,10 +8,32 @@ const Questions = ({
   response,
   selectCategory,
 }) => {
+  const [openResponse, setOpenResponse] = useState(false);
+
+  const handleOpenToResponse = () => {
+    setOpenResponse(!openResponse);
+  };
+
   return (
-    <div className="leading-9 cursor-pointer">
-      {selectCategory === category ? <p>{title}</p> : ""}
-    </div>
+    <>
+      <div className="leading-9 cursor-pointer flex w-full items-center">
+        {selectCategory === category ? (
+          <>
+            <p>{title}</p>{" "}
+            <img
+              onClick={() => handleOpenToResponse()}
+              src="../img/arrow.png"
+              alt="arrow"
+              className="w-[1.5%] h-3 ml-2 "
+            />{" "}
+          </>
+        ) : (
+          ""
+        )}
+      </div>
+
+      {openResponse ? <p>{response}</p> : ""}
+    </>
   );
 };
 
